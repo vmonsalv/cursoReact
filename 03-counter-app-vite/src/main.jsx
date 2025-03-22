40858b839513e42abc7a72e445904f7614dc4910
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-03-22 16:28:14 +0000

    hola mundo

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client"; // para renderizar
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// functional component
+const App = () => {
+    return <h1>Hola Mundo!!!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
