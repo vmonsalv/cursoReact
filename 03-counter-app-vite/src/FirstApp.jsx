9534e17ca7659ad4a49a866426db13a495da4069
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-03-29 17:41:13 -0300

    fragment

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,11 @@
+//fragment permite agrupar más de un elemento html bajo el mismo padre
+// se debe importar fragment de react
+// una alternatica es usar <></> (un sínonimo de fragment)
+
 export const FirstApp = () => {
-    return <h1>First App</h1>;
+    return <>
+        <h1>First App</h1>
+        <p>soy un subtitulo</p>    
+    </>
+    
 }
