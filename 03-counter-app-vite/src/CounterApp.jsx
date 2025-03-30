a2915e76d2b532f1f8325f4ba74c95f777a7ad09
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-03-29 22:40:01 -0300

    hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
+
+
 export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState(value);
+
+    // Esta función va dentro del functional component
+    // xq se trabajará con variables que están dentro del functional component
     const handleAdd = () => {
-        console.log('+1')
-        value = 1000
+        // console.log('+1')
+        setCounter(counter + 1)
+        // setCounter((c) => c + 1) -> otra manera de sumar 1
     }
 
     return <>
         <h1>CounterApp</h1>
-        <h2>{ value }</h2>
+        <h2>{ counter }</h2>
         <button onClick={ handleAdd }>
             +1
         </button>
