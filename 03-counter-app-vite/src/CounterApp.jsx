dac7407e51face25461e34f41760d9566ab44e9e
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-03-29 21:52:41 -0300

    evento onClinck

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,6 +1,14 @@
 export const CounterApp = ({ value }) => {
+    const handleAdd = () => {
+        console.log('+1')
+        value = 1000
+    }
+
     return <>
         <h1>CounterApp</h1>
         <h2>{ value }</h2>
+        <button onClick={ handleAdd }>
+            +1
+        </button>
     </>
 }
