36cd626078fb086a0b3e568107d0b166028ae55a
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-04-18 21:57:19 -0300

    jest types toBe

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -1,3 +1,12 @@
-test('Esta prueba no puede fallar', () => {
+describe('Pruebas de DemoComponent', () => {
     
+    test('Esta prueba no puede fallar', () => {
+        // 1. inicialización
+        const message1 = "Hola mundo";
+        // 2. estímulo
+        const message2 = message1.trim();
+    
+        // 3. observar comportamiento ... esperado
+        expect(message1).toBe(message2);
+    })
 })
