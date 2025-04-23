e73104080cde17d9a450d17b2f7cf26598948318
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-04-23 17:47:39 -0300

    renderizado gifs

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+    
+    const [images, setimages] = useState([])
 
     //No se debe poner el llamado a una función en el functional component
     // xq cada vez que se renderiza, se llama a la función
@@ -10,20 +12,33 @@ export const GifGrid = ({ category }) => {
     // useEffect es un hook de react que sirve para disparar efectos secundarios
     // un efecto secundario es un proceso que se quiere ejecutar cuando una acción suceda
     // un hook se identifica con el prefijo 'use'
+    // useEffect(() => {
+    //     /*const imgs = */getGifs(category).then(imgs =>setimages(imgs));
+    //     // setimages(imgs);
+    //     // return -> está destinado a hacer una limpieza si el useEffect tiene un observable
+    // }, []); // si se dejan las dependencias vacías ([]) solo se disparará una vez, cuando se crea el componente
+
+    const getImage = async () => {
+        const imgs = await getGifs(category);
+        setimages(imgs);
+    }
+
     useEffect(() => {
-        getGifs(category);
-        // return -> está destinado a hacer una limpieza si el useEffect tiene un observable
-    }, []); // si se dejan las dependencias vacías ([]) solo se disparará una vez, cuando se crea el componente
+        getImage();
+    }, []);
 
 
   return (
     <>
         <h3>{category}</h3>
-        {/* {
-            gifs.map(gif => (
-                <p>{gif}</p>
+        {
+            images.map(({ title, id, url}) => (
+                <>
+                <span>{title}</span>
+                <img key={id} src={url}/>
+                </>
             ))
-        } */}
+        }
     </>
   )
 }
