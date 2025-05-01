168f67da30c8bc36eef29f54fb02325689540bda
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-05-01 16:36:08 -0300

    custom hooks exponer métodos

---
+++ 05-hook-app/src/01-useState/CounterWithCustomHooks.jsx
@@ -0,0 +1,23 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterWithCustomHooks = () => {
+  const { counter, increment, decrement, reset } = useCounter();
+
+  return (
+    <>
+      <h1>CounterWithCustomHooks: {counter}</h1>
+
+      <hr />
+
+      <button className="btn btn-primary" onClick={() => increment(2)}>
+        +1
+      </button>
+      <button className="btn btn-primary" onClick={decrement}>
+        -1
+      </button>
+      <button className="btn btn-primary" onClick={() => reset(2)}>
+        Reset
+      </button>
+    </>
+  );
+};
