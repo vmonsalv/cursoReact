5c7618fcd74e99c212ae4a4bf9f49ad2b600e9e6
vmonsalv.vm@gmail.com <vmonsalv.vm@gmail.com>
2025-05-01 17:31:25 -0300

    useEffect unmount precauciones

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,17 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    console.log('Message mounted');
+    const onMouseMove = ({ x, y }) => {
+      // const coordenadas = { x, y };
+      setCoords({ x, y });
+    };
+    window.addEventListener("mousemove", onMouseMove);
 
     return () => {
-        console.log('Message unmounted');
+      // se debe tener cuidad de usar esta función
+      // xq en este caso al mover el mouse se usa el setCoords
+      // pero si el componente es desmontado se va a generar un error
+      // de intento de cambio de estado de un componente que no existe (v 16 - 17)
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
   return (
     <>
       <h3>Usuario ya existe</h3>
+
+      {JSON.stringify(coords)}
     </>
   );
 };
